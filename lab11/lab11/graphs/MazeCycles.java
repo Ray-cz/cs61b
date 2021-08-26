ab06668a844f1954ab3d7497a9667edff004da32
novRay <rayyc810@gmail.com>
2021-08-26 22:17:25 +0800

    finished lab11(Circle Check)

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,42 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private Maze maze;
+    private int[] pathTo;
+    private boolean circleFound = false;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        announce();
+        pathTo = new int[maze.V()];
+        dfs(0);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        for (int w : maze.adj(v)) {
+            if (circleFound) {
+                announce();
+                return;
+            }
+            if (!marked[w]) {
+                pathTo[w] = v;
+                dfs(w);
+            } else if (w != pathTo[v]) {
+                int cur = v;
+                pathTo[w] = v;
+                edgeTo[w] = v;
+                while (cur != w) {
+                    edgeTo[cur] = pathTo[cur];
+                    cur = pathTo[cur];
+                }
+                circleFound = true;
+            }
+        }
+    }
 }
 
