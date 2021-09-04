cfd9d572aa9474b17d9a848593f5ced8f999a168
novRay <rayyc810@gmail.com>
2021-09-04 16:15:27 +0800

    finished Graph Building

--- proj3/src/main/java/GraphBuildingHandler.java
+++ proj3/src/main/java/GraphBuildingHandler.java
@@ -2,9 +2,7 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  *  Parses OSM XML files using an XML SAX parser. Used to construct the graph of roads for
@@ -38,6 +36,9 @@ public class GraphBuildingHandler extends DefaultHandler {
                     "secondary_link", "tertiary_link"));
     private String activeState = "";
     private final GraphDB g;
+    private List<Long> wayNodes;
+    private GraphDB.Node currNode;
+    private GraphDB.Way currWay;
 
     /**
      * Create a new GraphBuildingHandler.
@@ -68,22 +69,29 @@ public class GraphBuildingHandler extends DefaultHandler {
         if (qName.equals("node")) {
             /* We encountered a new <node...> tag. */
             activeState = "node";
-//            System.out.println("Node id: " + attributes.getValue("id"));
-//            System.out.println("Node lon: " + attributes.getValue("lon"));
-//            System.out.println("Node lat: " + attributes.getValue("lat"));
-
-            /* TODO Use the above information to save a "node" to somewhere. */
-            /* Hint: A graph-like structure would be nice. */
+            //System.out.println("Node id: " + attributes.getValue("id"));
+            //System.out.println("Node lon: " + attributes.getValue("lon"));
+            //System.out.println("Node lat: " + attributes.getValue("lat"));
+            long id = Long.parseLong(attributes.getValue("id"));
+            double lon = Double.parseDouble(attributes.getValue("lon"));
+            double lat = Double.parseDouble(attributes.getValue("lat"));
+            currNode = new GraphDB.Node(id, lon, lat);
+            g.addNode(currNode);
 
         } else if (qName.equals("way")) {
             /* We encountered a new <way...> tag. */
             activeState = "way";
-//            System.out.println("Beginning a way...");
+            //System.out.println("Beginning a way...");
+
+            long id = Long.parseLong(attributes.getValue("id"));
+            currWay = new GraphDB.Way(id);
+            g.addWay(currWay);
+            wayNodes = new ArrayList<>();
         } else if (activeState.equals("way") && qName.equals("nd")) {
             /* While looking at a way, we found a <nd...> tag. */
             //System.out.println("Id of a node in this way: " + attributes.getValue("ref"));
-
-            /* TODO Use the above id to make "possible" connections between the nodes in this way */
+            long refNode = Long.parseLong(attributes.getValue("ref"));
+            wayNodes.add(refNode);
             /* Hint1: It would be useful to remember what was the last node in this way. */
             /* Hint2: Not all ways are valid. So, directly connecting the nodes here would be
             cumbersome since you might have to remove the connections if you later see a tag that
@@ -96,19 +104,28 @@ public class GraphBuildingHandler extends DefaultHandler {
             String v = attributes.getValue("v");
             if (k.equals("maxspeed")) {
                 //System.out.println("Max Speed: " + v);
-                /* TODO set the max speed of the "current way" here. */
+                currWay.maxSpeed = v;
             } else if (k.equals("highway")) {
                 //System.out.println("Highway type: " + v);
-                /* TODO Figure out whether this way and its connections are valid. */
+                if (ALLOWED_HIGHWAY_TYPES.contains(v)) {
+                    for (int i = 1; i < wayNodes.size(); i++) {
+                        Long prev = wayNodes.get(i - 1);
+                        Long curr = wayNodes.get(i);
+                        g.addAdj(prev, curr);
+                        g.addAdj(curr, prev);
+                    }
+                }
                 /* Hint: Setting a "flag" is good enough! */
             } else if (k.equals("name")) {
                 //System.out.println("Way Name: " + v);
+                currWay.name = v;
             }
 //            System.out.println("Tag with k=" + k + ", v=" + v + ".");
         } else if (activeState.equals("node") && qName.equals("tag") && attributes.getValue("k")
                 .equals("name")) {
             /* While looking at a node, we found a <tag...> with k="name". */
-            /* TODO Create a location. */
+            String location = attributes.getValue("v");
+            currNode.name = location;
             /* Hint: Since we found this <tag...> INSIDE a node, we should probably remember which
             node this tag belongs to. Remember XML is parsed top-to-bottom, so probably it's the
             last node that you looked at (check the first if-case). */
--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Graph for storing all of the intersection (vertex) and road (edge) information.
@@ -21,9 +21,11 @@ public class GraphDB {
     /** Your instance variables for storing the graph. You should consider
      * creating helper classes, e.g. Node, Edge, etc. */
 
+    private final Map<Long, Node> nodes = new HashMap<>();
+    private final Map<Long, Way> ways = new HashMap<>();
     /**
      * Example constructor shows how to create and start an XML parser.
-     * You do not need to modify this constructor, but you're welcome to do so.
+     * You do not need to modify this constructor, but you're welcMome to do so.
      * @param dbPath Path to the XML file to be parsed.
      */
     public GraphDB(String dbPath) {
@@ -57,7 +59,14 @@ public class GraphDB {
      *  we can reasonably assume this since typically roads are connected.
      */
     private void clean() {
-        // TODO: Your code here.
+        // use Iterator.remove method to avoid ConcurrentModificationException
+        Iterator<Long> iter = nodes.keySet().iterator();
+        while (iter.hasNext()) {
+            Long v = iter.next();
+            if (nodes.get(v).adj.isEmpty()) {
+                iter.remove();
+            }
+        }
     }
 
     /**
@@ -65,8 +74,7 @@ public class GraphDB {
      * @return An iterable of id's of all vertices in the graph.
      */
     Iterable<Long> vertices() {
-        //YOUR CODE HERE, this currently returns only an empty list.
-        return new ArrayList<Long>();
+        return nodes.keySet();
     }
 
     /**
@@ -75,7 +83,7 @@ public class GraphDB {
      * @return An iterable of the ids of the neighbors of v.
      */
     Iterable<Long> adjacent(long v) {
-        return null;
+        return nodes.get(v).adj;
     }
 
     /**
@@ -136,7 +144,17 @@ public class GraphDB {
      * @return The id of the node in the graph closest to the target.
      */
     long closest(double lon, double lat) {
-        return 0;
+        double minDistance = Double.MAX_VALUE;
+        long res = 0;
+        for (Long n : nodes.keySet()) {
+            Node v = nodes.get(n);
+            double dist = distance(lon, lat, v.lon, v.lat);
+            if (minDistance > dist) {
+                minDistance = dist;
+                res = n;
+            }
+        }
+        return res;
     }
 
     /**
@@ -145,7 +163,7 @@ public class GraphDB {
      * @return The longitude of the vertex.
      */
     double lon(long v) {
-        return 0;
+        return nodes.get(v).lon;
     }
 
     /**
@@ -154,6 +172,62 @@ public class GraphDB {
      * @return The latitude of the vertex.
      */
     double lat(long v) {
-        return 0;
+        return nodes.get(v).lat;
+    }
+
+    /**
+     * Adds a node to the graph
+     * @param n The node to be added
+     */
+    void addNode(Node n) {
+        nodes.put(n.id, n);
+    }
+
+    void addAdj(Long v1, Long v2) {
+        nodes.get(v1).adj.add(v2);
+    }
+
+    void removeNode(Long v) {
+        nodes.remove(v);
+    }
+
+    /**
+     * Adds a way to the graph
+     * @param w The way to be added
+     */
+    void addWay(Way w) {
+        ways.put(w.id, w);
+    }
+
+    static class Node {
+        long id;
+        double lon;
+        double lat;
+        String name;
+        Set<Long> adj = new HashSet<>();
+
+        public Node(long id, double lon, double lat) {
+            this.id = id;
+            this.lon = lon;
+            this.lat = lat;
+        }
+        public Node(long id, double lon, double lat, String name) {
+            this.id = id;
+            this.lon = lon;
+            this.lat = lat;
+            this.name = name;
+        }
+    }
+
+    static class Way {
+        long id;
+        String maxSpeed;
+        String name;
+
+        public Way(long id) {
+            this.id = id;
+        }
+
+
     }
 }
